ceb1b75b8f367a7efcc17d923029d44f517b5846
scurbys <maks_ponomarev_2003@mail.ru>
2024-09-09 22:39:41 +0500

    Update task2.c

--- labs/lab1/task2.c
+++ labs/lab1/task2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+  int a = 2, b = 3;
+  int c = a + b;
+  printf("c=%d\n", c);
+
+  a = 5; b = 7;
+  c = a + b;
+  printf("c=%d\n", c);
+
+  a = 10; b = -2;
+  c = a + b;
+  printf("c=%d\n", c);
+
+  return 0;
+}
